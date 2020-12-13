a2f4c805191df59bf4860fadffa2ddee95a8f72d
Lee Rogers <edwinleerogers@gmail.com>
2020-12-12 17:49:06 -0800

    add secretWord action creator and reducer

--- src/reducers/secretWordReducer.js
+++ src/reducers/secretWordReducer.js
@@ -1,4 +1,11 @@
-export default function secretWordReducer(state=null, action) {
+import { actionTypes } from "../actions";
 
+export default function secretWordReducer(state=null, action) {
+  switch (action.type) {
+    case actionTypes.SET_SECRET_WORD:
+      return action.payload;
+    default:
+      return state;
+  }
   return state;
 }
